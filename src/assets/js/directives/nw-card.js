bbe3b95cb407c362a56763ee86dd2a1128e207f0
Barth <ceul1barth@hotmail.fr>
2015-12-17 00:35:29 +0100

    change scope to isolate scope in nw-card directive

--- src/assets/js/directives/nw-card.js
+++ src/assets/js/directives/nw-card.js
@@ -2,9 +2,9 @@ angular.module('NoteWrangler').directive('nwCard',[function(){
     return {
 	restrict: 'E',
 	templateUrl: '../../../templates/directives/nw-card.html',
-	controller: function($scope){
-	    $scope.header = "Note title";
-	    $scope.description = "A lovely note description.";
-	},
+	scope: {
+	    header: "=",
+	    description: "="
+	}
     };
 }])
