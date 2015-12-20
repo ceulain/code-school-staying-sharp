2f24284a22a34c09aac7ad641d94b3473186e347
Barth <ceul1barth@hotmail.fr>
2015-12-20 21:11:30 +0100

    add bar max character for tweetable service

--- src/assets/js/services/tweetable.js
+++ src/assets/js/services/tweetable.js
@@ -1,10 +1,14 @@
 angular.module('NoteWrangler')
     .factory('Tweetable', ['$http', function TweetableFactory($http){
+	var characterLength = 144;
 	return function(potentialTweet){
 	    return $http({
 		method: 'POST',
 		url: 'ttp://gentle-spire-1153.herokuapp.com/tweet',
-		data : { description : potentialTweet }
+		data : { 
+		    description: potentialTweet,
+		    maxLength: characterLength
+		}
 	    });
 	};
     }]);
