7c6c067e387ade140ad685eb8aaee00698922a1a
Barth <ceul1barth@hotmail.fr>
2015-12-22 23:19:16 +0100

    add route & new file tweeted.html

--- src/assets/js/route.js
+++ src/assets/js/route.js
@@ -6,22 +6,33 @@ angular.module('NoteWrangler', ['ngRoute'])
     		redirectTo: '/users'
   	    })
 	    .when('/notes', {
-		templateUrl: 'templates/pages/notes/index.html',
+		templateUrl: '../../../templates/pages/notes/index.html',
 		controller: 'NotesIndexController'
 	    })
 	    .when('/users', {
-		templateUrl: 'templates/pages/users/index.html'
+		templateUrl: '../../../templates/pages/users/index.html'
+	    })
+	    .when('/users/:id', {
+		templateUrl: '../../../templates/pages/users/show.html',
+		controller: 'UsersShowController'
 	    })
   	    .when('/notes/new', {
-    		templateUrl: 'templates/pages/notes/edit.html',
+    		templateUrl: '../../../templates/pages/notes/edit.html',
 		controller: 'NotesCreateController',
 		controllerAs: 'notesEditCtrl'
 	    })
 	    .when('/notes/:id', {
-		templateUrl: 'templates/pages/notes/show.html',
+		templateUrl: '../../../templates/pages/notes/show.html',
 		controller: 'NotesShowController'
 	    })
-  	
+	    .when('/notes/:id/edit', {
+		templateUrl: '../../../templates/pages/notes/edit.html',
+		controller: 'NotesEditController'
+	    })
+  	    .when('/tweeted', {
+		templateUrl: '../../../templates/pages/notes/tweeted.html',
+		controller: 'TweetedController'
+	    })
   	    .otherwise({ redirectTo: '/notes'});
 
     }]);
